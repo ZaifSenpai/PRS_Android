b943d878bea6181fcd3abe1289f38408db9dbb1f
Zaif Senpai <escbutt@gmail.com>
2019-05-02 03:12:16 +0500

    Changed theme color and updated icon. Also changed validation errors on login and signup forms from Snack Bar to use setError() instead.

--- app/src/main/java/zaifsenpai/prs/Welcome/SignInActivity.java
+++ app/src/main/java/zaifsenpai/prs/Welcome/SignInActivity.java
@@ -2,9 +2,7 @@ package zaifsenpai.prs.Welcome;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -36,29 +34,14 @@ public class SignInActivity extends Activity {
             public void onClick(View v) {
                 EditText u_name = findViewById(R.id.ET_SignIn_Username);
                 EditText password = findViewById(R.id.ET_SignIn_Password);
-                String error_text = "";
 
                 if (u_name.getText().length() < _Properties.USERNAME_MIN_LENGTH)
-                    error_text = getString(R.string.login_invalid_username);
+                    u_name.setError(getString(R.string.login_invalid_username));
                 if (password.getText().length() < _Properties.PASSWORD_MIN_LENGTH)
-                    if (error_text.length() > 0)
-                        error_text += "\n" + getString(R.string.login_invalid_password);
-                    else
-                        error_text += getString(R.string.login_invalid_password);
+                    password.setError(getString(R.string.login_invalid_password));
 
-                if (error_text.length() > 0) {
-                    Snackbar sb = Snackbar.make(v, error_text, Snackbar.LENGTH_INDEFINITE);
-                    sb.setAction("RETRY", new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                        }
-                    });
-                    sb.setActionTextColor(Color.RED);
-                    View sbView = sb.getView();
-                    sbView.setBackgroundColor(Color.WHITE);
-                    sb.show();
-                }
-                else {
+                if (u_name.getError() == null && password.getError() == null)
+                {
                     Intent it = new Intent(SignInActivity.this, MainActivity.class);
                     startActivity(it);
                 }
